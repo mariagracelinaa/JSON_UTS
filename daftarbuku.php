bbea6a57bbac595b819a39c41c927777062ff2a7
mariagracelinaa <96014335+mariagracelinaa@users.noreply.github.com>
2022-03-22 15:57:08 +0700

    Create daftarbuku.php

---
+++ daftarbuku.php
@@ -0,0 +1,12 @@
+[
+    { "id": "bk001", "judul":"Daughter of the deep", "penulis": "Rick Riordan", "tahun": "2021", "photo_url": "https://picsum.photos/400/500"}, 
+    { "id": "bk002", "judul":"Semua Bisa Menjadi Programmer Laravel Basic", "penulis": "Yuniar Supardi, Sulaeman", "tahun": "2019", "photo_url": "https://picsum.photos/400/500"},
+    { "id": "bk003", "judul":"Dear Nathan", "penulis": "Erisca Febriani", "tahun": "2016", "photo_url": "https://picsum.photos/400/500"},
+    { "id": "bk004", "judul":"Harry Potter and the Philosopher's Stone", "penulis": "J.K. Rowling", "tahun": "1997", "photo_url": "https://picsum.photos/400/500"},
+    { "id": "bk005", "judul":"The Tower of Nero", "penulis": "Rick Riordan", "tahun": "2020", "photo_url": "https://picsum.photos/50"},
+    { "id": "bk006", "judul":"Belajar JavaScript Menggunakan JQUERY", "penulis": "Wahana Komputer", "tahun": "2021", "photo_url": "https://picsum.photos/400/500"},
+    { "id": "bk007", "judul":"Podkin One-Ear", "penulis": "Kieran Larwood", "tahun": "2022", "photo_url": "https://picsum.photos/400/500"},
+    { "id": "bk008", "judul":"Beren And Lúthien", "penulis": "J.R.R. Tolkien", "tahun": "2022", "photo_url": "https://picsum.photos/400/500"},
+    { "id": "bk009", "judul":"Kisah-Kisah Anjing (Dog Stories)", "penulis": "James Herriot", "tahun": "2019", "photo_url": "https://picsum.photos/400/500"},
+    { "id": "bk010", "judul":"Harry Potter and the Half-Blood Prince", "penulis": "J.K. Rowling", "tahun": "2005", "photo_url": "https://picsum.photos/400/500"}
+]
